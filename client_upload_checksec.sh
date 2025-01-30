737b713159f813dca9f6b76f23f894b107e3c047
Joakim Brorsson <joakim@hyker.io>
2025-01-30 14:10:54 +0100

    fixed curl payload size issue

--- client_upload_checksec.sh
+++ client_upload_checksec.sh
@@ -1,9 +1,7 @@
 #curl -X POST -d "@testdata/example.cpp" https://10.1.6.16:8443/upload --cacert rte_keys/cert_ca.pem
 
 TOE=$(base64 testdata/a.out -w 0)
-#echo $TOE
-
-curl -X POST -H "Content-Type: application/json" -d '{
+echo '{
   "toe": {
     "format": "string",
     "base64_encoded_toe": "'"$TOE"'"
@@ -14,4 +12,8 @@ curl -X POST -H "Content-Type: application/json" -d '{
       "parameters": null
     }
   ]
-}' https://10.1.6.16:8443/upload --cacert cert_ca.pem
+}' >payload.data
+
+curl -X POST -H "Content-Type: application/json" -d @payload.data https://10.1.6.16:8443/upload --cacert cert_ca.pem
+
+rm payload.data
