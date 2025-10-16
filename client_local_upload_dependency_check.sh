fd77ebde7e501f2d98efbc2baf5dbd92a2701896
Joakim Brorsson <joakim.brorsson@hyker.se>
2025-10-16 09:09:48 +0200

    specifying file type on upload

--- client_local_upload_dependency_check.sh
+++ client_local_upload_dependency_check.sh
@@ -1,3 +1,3 @@
 base64 testdata/commons-collections-3.2.1.jar -w 0 | \
-  jq -R '{toe: {format: "string", base64_encoded_toe: .}, test: {tool_name: "dependency-check", parameters: [{param_name: "--format=", value: "JSON"}]}}' | \
+  jq -R '{toe: {format: "string", base64_encoded_toe: .}, test: {tool_name: "dependency-check", parameters: [{param_name: "--format=", value: "JSON"}, {param_name: "--file-type=", value: "jar"}]}}' | \
   curl -X POST -H "Content-Type: application/json" -d @- https://localhost:8443/upload -k
--- client_upload_dependency_check.sh
+++ client_upload_dependency_check.sh
@@ -1,3 +1,3 @@
 base64 testdata/commons-collections-3.2.1.jar -w 0 | \
-  jq -R '{toe: {format: "string", base64_encoded_toe: .}, test: {tool_name: "dependency-check", parameters: [{param_name: "--format=", value: "JSON"}]}}' | \
+  jq -R '{toe: {format: "string", base64_encoded_toe: .}, test: {tool_name: "dependency-check", parameters: [{param_name: "--format=", value: "JSON"}, {param_name: "--file-type=", value: "jar"}]}}' | \
   curl -X POST -H "Content-Type: application/json" -d @- https://10.1.6.16:8443/upload --cacert cert_ca.pem
